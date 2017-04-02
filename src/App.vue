5d1e00fb64bd8c420c3c64e3801a9c6a0b0d4542
Marc <mrmarcmartino@gmail.com>
2017-04-02 16:05:35 -0700

    Added an schedule item class
    still have to load from ajax and get editing into the template

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,42 @@
   <div id="app">
     <img src="./assets/logo.png">
     <banner></banner>
+    <service-schedule-item v-for='item in scheduleList' v-bind:service="item">
+          </service-schedule-item>
   </div>
 </template>
 
 <script>
 import Banner from './components/Banner'
+import ServiceScheduleItem from './components/ServiceScheduleItem'
 
 export default {
   name: 'app',
   components: {
-    Banner
+    Banner,
+    ServiceScheduleItem
+  },
+  data () {
+    return {
+      scheduleList: [
+        {
+          'id': 33491,
+          'commandText': 'rake do_something',
+          'dynoSize': '1X',
+          'frequency': 'daily',
+          'lastRun': 'never',
+          'nextDue': 1491010288018
+        },
+        {
+          'id': 33492,
+          'commandText': 'rake test something',
+          'dynoSize': '1X',
+          'frequency': 'daily',
+          'lastRun': 'yesterday',
+          'nextDue': 1491011288018
+        }
+      ]
+    }
   }
 }
 </script>
---
+++ src/components/ServiceScheduleItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="service-schedule-item">
+    <div>
+      <h2>{{service.commandText}}</h2>
+      <div> {{service.dynoSize}} - {{service.frequency}} - {{service.lastRun}} - {{service.nextDue | prettyUtcDate}}</div>
+      <button>Edit</button>
+      <button>Remove</button>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'service-schedule-item',
+  props: ['service'],
+  filters: {
+    prettyUtcDate: function (val) {
+      return (new Date(val)).toUTCString()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
