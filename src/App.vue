a1014cdb57922ebf18b1b8cd206911a6b656caab
Marc <mrmarcmartino@gmail.com>
2017-04-02 15:43:22 -0700

    Initial commit
    Ran the scaffolding and modified the hello comp

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <banner></banner>
+  </div>
+</template>
+
+<script>
+import Banner from './components/Banner'
+
+export default {
+  name: 'app',
+  components: {
+    Banner
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
